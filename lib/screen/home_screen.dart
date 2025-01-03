b5800492aed3f99b0f2eb456c3748ef081d32097
Only One Kidney <Kevinli890404@gmail.com>
2025-01-04 01:44:47 +0800

    新增冒險者日誌，接續為抽卡與商城

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -36,15 +36,20 @@ class _HomeScreenState extends State<HomeScreen> {
                             horizontal: 12.0, vertical: 6.0),
                         child: Row(
                           children: [
-                            CircleAvatar(
-                              radius: 20,
-                              backgroundColor: Colors.white,
-                              child: ClipOval(
-                                child: Image.asset(
-                                  'assets/character_pictures/1.jpg',
-                                  fit: BoxFit.cover,
-                                  width: 40,
-                                  height: 40,
+                            GestureDetector(
+                              onTap: () {
+                                _showProfileDialog(context);
+                              },
+                              child: CircleAvatar(
+                                radius: 20,
+                                backgroundColor: Colors.white,
+                                child: ClipOval(
+                                  child: Image.asset(
+                                    'assets/character_pictures/1.jpg',
+                                    fit: BoxFit.cover,
+                                    width: 40,
+                                    height: 40,
+                                  ),
                                 ),
                               ),
                             ),
@@ -161,128 +166,6 @@ class _HomeScreenState extends State<HomeScreen> {
                                             ),
                                           ),
                                         ),
-                                        const SizedBox(width: 8),
-                                        GestureDetector(
-                                          onTap: () {
-                                            sharedState.toggleDrawed();
-                                            sharedState.updateCurrentIndex(3);
-                                          },
-                                          child: Container(
-                                            width: 70,
-                                            decoration: BoxDecoration(
-                                              color: Colors.amber.shade50,
-                                              borderRadius:
-                                                  BorderRadius.circular(20),
-                                              boxShadow: [
-                                                BoxShadow(
-                                                  color: Colors.black
-                                                      .withOpacity(0.2),
-                                                  blurRadius: 5,
-                                                  offset: const Offset(0, 3),
-                                                ),
-                                              ],
-                                            ),
-                                            padding: const EdgeInsets.symmetric(
-                                                vertical: 4.0, horizontal: 8.0),
-                                            child: Column(
-                                              mainAxisSize: MainAxisSize.min,
-                                              children: [
-                                                const Text(
-                                                  '祈願1次',
-                                                  style: TextStyle(
-                                                    color: Color(0xFF8D6E63),
-                                                    fontSize: 12,
-                                                    fontWeight: FontWeight.bold,
-                                                  ),
-                                                ),
-                                                const SizedBox(height: 2),
-                                                Row(
-                                                  mainAxisAlignment:
-                                                      MainAxisAlignment.center,
-                                                  children: [
-                                                    Image.asset(
-                                                      'assets/logo/gen_stone.png',
-                                                      width: 12,
-                                                      height: 12,
-                                                    ),
-                                                    const SizedBox(width: 2),
-                                                    const Text(
-                                                      'x 10',
-                                                      style: TextStyle(
-                                                        color:
-                                                            Color(0xFF8D6E63),
-                                                        fontSize: 12,
-                                                        fontWeight:
-                                                            FontWeight.bold,
-                                                      ),
-                                                    ),
-                                                  ],
-                                                ),
-                                              ],
-                                            ),
-                                          ),
-                                        ),
-                                        const SizedBox(width: 8),
-                                        GestureDetector(
-                                          onTap: () {
-                                            sharedState.toggleDrawed();
-                                            sharedState.updateCurrentIndex(4);
-                                          },
-                                          child: Container(
-                                            width: 70,
-                                            decoration: BoxDecoration(
-                                              color: Colors.amber.shade50,
-                                              borderRadius:
-                                                  BorderRadius.circular(20),
-                                              boxShadow: [
-                                                BoxShadow(
-                                                  color: Colors.black
-                                                      .withOpacity(0.2),
-                                                  blurRadius: 5,
-                                                  offset: const Offset(0, 3),
-                                                ),
-                                              ],
-                                            ),
-                                            padding: const EdgeInsets.symmetric(
-                                                vertical: 4.0, horizontal: 8.0),
-                                            child: Column(
-                                              mainAxisSize: MainAxisSize.min,
-                                              children: [
-                                                const Text(
-                                                  '祈願10次',
-                                                  style: TextStyle(
-                                                    color: Color(0xFF8D6E63),
-                                                    fontSize: 12,
-                                                    fontWeight: FontWeight.bold,
-                                                  ),
-                                                ),
-                                                const SizedBox(height: 2),
-                                                Row(
-                                                  mainAxisAlignment:
-                                                      MainAxisAlignment.center,
-                                                  children: [
-                                                    Image.asset(
-                                                      'assets/logo/gen_stone.png',
-                                                      width: 12,
-                                                      height: 12,
-                                                    ),
-                                                    const SizedBox(width: 2),
-                                                    const Text(
-                                                      'x 100',
-                                                      style: TextStyle(
-                                                        color:
-                                                            Color(0xFF8D6E63),
-                                                        fontSize: 12,
-                                                        fontWeight:
-                                                            FontWeight.bold,
-                                                      ),
-                                                    ),
-                                                  ],
-                                                ),
-                                              ],
-                                            ),
-                                          ),
-                                        ),
                                       ],
                                     ),
                                   ),
@@ -293,6 +176,141 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ],
               ),
+      ),
+    );
+  }
+
+  void _showProfileDialog(BuildContext context) {
+    final serverAPI = Provider.of<ServerAPI>(context, listen: false);
+
+    // 獲取角色數據
+    final characterData = serverAPI.characterData;
+
+    showDialog(
+      context: context,
+      barrierDismissible: true, // 點擊外部關閉對話框
+      builder: (BuildContext context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              color: const Color(0xFF8D6E63), // 外層深棕色
+              borderRadius: BorderRadius.circular(20),
+            ),
+            padding: const EdgeInsets.all(4), // 深棕色邊框的厚度
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.amber.shade50, // 內層淺棕色
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SizedBox(
+                  width: 350,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min, // 佔用所需高度
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Center(
+                        child: Text(
+                          "冒險者日誌",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      characterData == null
+                          ? const Center(
+                              child: Text("無角色數據"),
+                            )
+                          : Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    SizedBox(
+                                      width: 100, // 左列寬度
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          _buildLabel("角色名"),
+                                          _buildLabel("VIP等級"),
+                                          _buildLabel("創建時間"),
+                                          _buildLabel("寶石數量"),
+                                          _buildLabel("首充狀態"),
+                                        ],
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 200, // 右列寬度
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          _buildValue(
+                                              characterData.characterName),
+                                          _buildValue(
+                                            characterData.vipLevel.toString(),
+                                            color: const Color(
+                                                0xFFFFD700), // 設置為金色
+                                          ),
+                                          _buildValue(characterData.createTime),
+                                          _buildValue(
+                                              characterData.gem.toString()),
+                                          _buildValue(
+                                            characterData.topUpState == 1
+                                                ? "已完成"
+                                                : "未完成",
+                                            color: characterData.topUpState == 1
+                                                ? Colors.black
+                                                : Colors.red, // 設置紅色
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  // 創建標籤方法
+  Widget _buildLabel(String text) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Text(
+        "$text：",
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 14,
+        ),
+      ),
+    );
+  }
+
+  // 創建值方法，支持可選顏色
+  Widget _buildValue(String text, {Color color = Colors.black}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: 14,
+          color: color,
+        ),
       ),
     );
   }
